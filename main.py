74198e68615693e5906c43d9c2afb07d29892750
David Lechón Quiñones <dalequi@gmail.com>
2013-09-12 11:38:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def main():
     Player1 = 0
     Player2 = 0 
 
-    player1 = TennisPlayer(float(input('Player 1 - % First Serve: ')),float(input('Player 1 - % First Serve Points Won: ')), float(input('Player 1 - % Second Serve Points Won: ')), float(input('Player 1 - % First Serve Return Points Won: ')),  float(input('Player 1 - % Second Serve Return Points Won: ')))
-    player2 = TennisPlayer(float(input('Player 2 - % First Serve: ')),float(input('Player 2 - % First Serve Points Won: ')), float(input('Player 2 - % Second Serve Points Won: ')), float(input('Player 2 - % First Serve Return Points Won: ')),  float(input('Player 2 - % Second Serve Return Points Won: ')))
+    player1 = TennisPlayer(float(input('Player 1 - First Serve: ')),float(input('Player 1 - First Serve Points Won: ')), float(input('Player 1 - Second Serve Points Won: ')), float(input('Player 1 - First Serve Return Points Won: ')),  float(input('Player 1 - Second Serve Return Points Won: ')))
+    player2 = TennisPlayer(float(input('Player 2 - First Serve: ')),float(input('Player 2 - First Serve Points Won: ')), float(input('Player 2 - Second Serve Points Won: ')), float(input('Player 2 - First Serve Return Points Won: ')),  float(input('Player 2 - Second Serve Return Points Won: ')))
 
     number_of_sets = int(input('How many sets for each match? '))
     number_of_simulations = int(input('How many simulations do you want to reproduce? '))
@@ -16,8 +16,8 @@ def main():
         else:
             Player2 += 1
 
-    print "Player 1 won " + str(Player1) + " - " + str(float(Player1)/number_of_simulations) + "%"
-    print "Player 2 won " + str(Player2) + " - " + str(float(Player2)/number_of_simulations) + "%"
+    print "Player 1 won " + str(Player1) + " - " + str((float(Player1)/number_of_simulations)*100) + "%"
+    print "Player 2 won " + str(Player2) + " - " + str((float(Player2)/number_of_simulations)*100) + "%"
 
 
 if __name__ == "__main__":
